7026d3fe859ec0fd0e232b6edf9b098814bc0ec6
mkhululi <mkhululimtshali@gmail.com>
2023-03-31 14:24:56 +0200

    initial greetings function test commit

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,7 +1,14 @@
-describe("this test", function () {
-  it("should show you how to use MochaJS", function () {
-    assert.equal(2, 2);
-
-    assert.deepEqual([2, 2], [2, 2]);
+describe("Test My Greet Function", function () {
+  it("it should return 'Hello, Mkhululi, when called with Mkhululi'", function () {
+    assert.equal(`Hello, mkhululi`, greet("Mkhululi"));
+  });
+  it("it should return 'Hello, Mkhululi, when called with Mkhululi'", function () {
+    assert.equal(`Hello, mkhululi`, greet("mkululi"));
+  });
+  it("it should return 'Hello, Mkhululi, when called with Mkhululi'", function () {
+    assert.equal(`Hello, mkhululi`, greet(""));
+  });
+  it("it should return 'Hello, Mkhululi, when called with Mkhululi'", function () {
+    assert.equal(`Hello, mkhululi`, greet("Mkhulul1"));
   });
 });
